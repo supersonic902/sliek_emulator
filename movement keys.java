6fb592c2039f5bac6ccec5cafa51990969524493
supersonic902 <dalton.harmon921@gaston.k12.nc.us>
2015-09-15 14:16:49 -0400

    Create movement keys.java

---
+++ movement keys.java
@@ -0,0 +1,7 @@
+<canvas>
+  z16o   //this is the bit processer
+    var.control(up) = input.key.(w), var.control(down) = input.key.(s), var.control(left) = input.key.(a), var.control(right) = input.key.(d)
+  if  var.control(up) = true
+    then int <sprite.control.trigger(up)>
+  if <control> = down
+    then int <control.trigger(down)>
